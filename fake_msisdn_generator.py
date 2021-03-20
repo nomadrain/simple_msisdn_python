1de7b607fff4f8c600dba45b141a37dbab34c92a
Nomad Rain <nomadrain@gmail.com>
2021-03-20 15:36:51 +0200

    Next commit 1616247411

--- fake_msisdn_generator.py
+++ fake_msisdn_generator.py
@@ -22,14 +22,17 @@ class FakeMsisdnGenerator():
     :param allowed_country_codes - Iterable of strings
     :param allowed_national_destination_codes - Iterable of strings
     :param subscriber_number_length - scalar int
-    :param preferable_ndc - National destination code which will be selected with %90 probability
+    :param preferable_ndc - National destination code which will be selected with the probability given
+        in the preferable_prob parameter
+    :param preferable_prob. Int. Probability of choosing the preferable_ndc as an NDC for the generated MSISDN
     """
 
     def __init__(self,
                  allowed_country_codes=('+000', '+111'),
                  allowed_national_destination_codes=('11', '22', '33'),
                  subscriber_number_length=7,
-                 preferable_ndc='50'
+                 preferable_ndc='50',
+                 preferable_prob=80
                  ):
         if allowed_country_codes:
             self.allowed_country_codes = allowed_country_codes
@@ -49,7 +52,12 @@ class FakeMsisdnGenerator():
         if preferable_ndc:
             self.preferable_ndc = preferable_ndc
         else:
-            preferable_ndc = '50'
+            self.preferable_ndc = '50'
+
+        if preferable_prob and type(preferable_prob) is int:
+            self.preferable_prob = int(preferable_prob)
+        else:
+            self.preferable_prob = 80
 
     def get(self):
         """
@@ -58,7 +66,8 @@ class FakeMsisdnGenerator():
         """
         random.seed(time())
         cc = random.choice(self.allowed_country_codes)
-        ndc = random.choices((self.preferable_ndc, random.choice(self.allowed_national_destination_codes)), weights=(90,10), k=1)[0]
+        ndc = random.choices((self.preferable_ndc, random.choice(self.allowed_national_destination_codes)),
+                             weights=(self.preferable_prob, 100 - self.preferable_prob), k=1)[0]
         sn = ''.join(random.choices(string.digits, k=self.subscriber_number_length))
         return SimpleMSISDN(country_code=cc, national_destination_code=ndc, subscriber_number=sn)
 
